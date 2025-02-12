efea6d5338fc136600abb00cd8db64803376543d
saidAbdirahman <saidqaaje11@gmail.com>
2025-02-12 07:23:22 +0100

    set the state
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+  let [star1, setStar1] = useState(false)
+  let [star2, setStar2] = useState(false)
+  let [star3, setStar3] = useState(false)
+  let [star4, setStar4] = useState(false)
+  let [star5, setStar5] = useState(false)
+
+ 
+  
+  
+  return (
+
+   <h1>hello</h1>
+  )
+}
+
+export default App
